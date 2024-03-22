11f0698f3538a40e2be225a8449a9a0aa0a2092a
Valeria Spektor <vlapin84@gmail.com>
2024-03-22 09:00:22 +0200

    added commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,32 +10,41 @@
 //
 //
 // -- This is a parent command --
- Cypress.Commands.add('login', (email, password) => { ... })
-//cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc')
-cy.get('#account-menu').click()
-cy.get('#login-item').click()
-cy.get('input[name="username"]').type('student');
-cy.get('input[name="password"]').type('123456!');
-cy.get('button[type="submit"]').click();
+// Cypress.Commands.add('login', (email, password) => { ... })
+it('Should login with predefined credentials', () => {
+cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc');
+    cy.get('#account-menu').click();
+    cy.get('#login-item').click();
+    cy.get('input[name="username"]').type('student');
+    cy.get('input[name="password"]').type('123456!');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc');
+    cy.contains('Sqlverifier').should('be.visible').click();
+});
+
 //
 // -- This is a child command --
-Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-cy.get('.draggable-element').drag({ x: 100, y: 50 });
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
 //
 // -- This is a dual command --
-Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-cy.get('.modal-close-button').dismiss();
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
 //
 // -- This will overwrite an existing command --
-Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.overwrite('visit', (originalFn, url, options) => {
-    
-    cy.request(url).then((response) => {
-      if (response.status === 200) {
-        originalFn(url, options);
-      } else {
-        
-        cy.log(`Page ${url} is not available`);
-      }
-    });
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('switchLanguage', (language, expectedText) => {
+    cy.get('selectorForLanguageDropdown').click(); // Замените 'selectorForLanguageDropdown' на селектор вашего выпадающего меню с языками
+    cy.contains(language).click(); // Здесь 'language' должен быть текстом языка, который вы хотите выбрать
+    cy.contains(expectedText).should('be.visible'); // Проверяем, что текст, ожидаемый после смены языка, видим
   });
+  Cypress.Commands.add('logout', () => {
+    // Ваш код для выхода из аккаунта
+  });
+  Cypress.Commands.add('switchLanguage', (language, expectedText) => {
+    cy.get('#language-dropdown').click(); // Предположим, что для переключения языка используется выпадающее меню с id "language-dropdown"
+    cy.contains(language).click(); // Предположим, что название языка отображается в виде текста и соответствует значению варианта переключения языка
+    cy.contains(expectedText).should('be.visible'); // Проверяем, что ожидаемый текст отображается на странице после переключения языка
+  });
+      
+  
