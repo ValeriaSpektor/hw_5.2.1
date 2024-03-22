16dd6b72c3c3448f6f5b1a43ac7aeddef53ad3c2
Valeria Spektor <vlapin84@gmail.com>
2024-03-22 20:33:49 +0200

    added some changes

--- cypress-staging.config.js
+++ cypress-staging.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://sqlverifier-staging-08050d656f7a.herokuapp.com",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
     },
   },
 });
