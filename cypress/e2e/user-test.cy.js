a168617f30af5b96bc943a14857f057f5977afea
Valeria Spektor <vlapin84@gmail.com>
2024-03-22 08:59:55 +0200

    changed test

--- cypress/e2e/user-test.cy.js
+++ cypress/e2e/user-test.cy.js
@@ -1,118 +1,109 @@
-describe('Page Tests', () => {
-//1.Authorisation
-it("Should log in with predefined credentials", () => {
-  const login = "student";
-  const password = "123456!";
-})
-})
-//2.Load page
-describe('After Login', () => {
-  it('Should load the page successfully', () => {
+//1.Load page
+describe('Page Load Test', () => {
+  it('Should load the Verifier website successfully', () => {
     cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc')
     cy.url().should('eq', 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc')
     cy.contains('Sqlverifier').should('be.visible') 
   })
 })
- 
+describe('Page Tests', () => {
+//2.Authorisation
+it("Should log in with predefined credentials", () => {
+  const login = "student";
+  const password = "123456!";
+})
+})
+//3.Login test
+describe('Login Test', () => {
+  it('Should login with predefined credentials', () => {
+    cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc');
+    cy.get('#account-menu').click();
+    cy.get('#login-item').click();
+    cy.get('input[name="username"]').type('student');
+    cy.get('input[name="password"]').type('123456!');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc');
+    cy.contains('Sqlverifier').should('be.visible').click();
+  });
+});
 
-  //3.Login test
-  describe('Login Test', () => {
-    it('Should login with predefined credentials', () => {
-      cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc')
+  //4.User menu
+  describe('User Menu Tests', () => {
+    beforeEach(() => {
+      cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc'); // Переход на главную страницу перед каждым тестом
       cy.get('#account-menu').click()
       cy.get('#login-item').click()
       cy.get('input[name="username"]').type('student');
       cy.get('input[name="password"]').type('123456!');
       cy.get('button[type="submit"]').click();
-      cy.url().should('include', 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc')
-      cy.contains('Sqlverifier').should('be.visible').click()
     })
-  })
+    
+     //5.Button Home
+     describe('Header Menu Home', () => {
+      it('Should navigate to Home page from Header Menu', () => {
+        cy.get('#header-tabs').click();
+        cy.get('#header-tabs').should('be.visible');
+        cy.url().should('include', '/?page=1&sort=id,asc');
+        })
+      });
+    });
+//6.Task
+        describe('Login Test', () => {
+    it('Should navigate to Task page from Entities menu', () => {
+     
+        cy.get('#entity-menu').click();
+        cy.get('#entity-menu > div > a:nth-child(1)').click();
+        cy.url().should('include', '/task?page=1&sort=id,asc');
+        cy.get('#task-heading').should('include.text', 'Tasks');
+    });
+//7.User Task
+    it('Should navigate to User Task page from Entities menu', () => {
+        
+        cy.get('#entity-menu').click();
+        cy.get('#entity-menu > div > a:nth-child(2)').click();
+        cy.url().should('include', '/user-task');
+        cy.get('#user-task-heading').should('include.text', 'User Tasks');
+    });
 
-//4. Button Home
-it('Should click on the Home button', () => {
-  cy.contains('Sqlverifier').click();
-  cy.contains('Home').click();
-  cy.url().should('include', 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/');
-});
-
-//5. Button Task
-it('Should click on the Task button in the Entities menu', () => {
-  cy.contains('Sqlverifier').click(); 
-  cy.contains('Entities').click();
-  cy.contains('Task').click();
-  cy.url().should('include', 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/task?page=1&sort=id,asc');
-});
-
-//6. Button User Task
-it('Should click on the User Task button in the Entities menu', () => {
-  cy.contains('Sqlverifier').click();
-  cy.contains('Entities').click(); 
-  cy.contains('User Task').click();
-  cy.url().should('include', 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/user-task');
-});
-
-//7. Button English
-it('Should click on the English button in the English menu', () => {
-  cy.contains('Sqlverifier').click();
-  cy.contains('English').click(); 
-  cy.contains('English').click();
-  cy.url().should('include', 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc');
-});
-
-//8. Button Français
-it('Should click on the Français button in the English menu', () => {
-  cy.contains('Sqlverifier').click();
-  cy.contains('English').click(); 
-  cy.contains('Français').click();
-  cy.url().should('include', 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc');
-});
-
-//9. Button Russian
-it('Should click on the Russian button in the Français menu', () => {
-  cy.contains('Sqlverifier').click();
-  cy.contains('Français').click(); 
-  cy.contains('Russian').click();
-  cy.url().should('include', 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc');
-});
-
-//10. Button Українська
-it('Should click on the Українська button in the Russian menu', () => {
-  cy.contains('Sqlverifier').click();
-  cy.contains('Russian').click(); 
-  cy.contains('Українська').click();
-  cy.url().should('include', 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc');
-});
-
-//10. Button Settings
-it('Should click on the Settings button in the Account menu', () => {
-  cy.contains('Sqlverifier').click();
-  cy.contains('Account').click(); 
-  cy.contains('Settings').click();
-  cy.url().should('include', '/account/settings');
-});
-
-//11. Button Password
-it('Should click on the Password button in the Account menu', () => {
-  cy.contains('Sqlverifier').click();
-  cy.contains('Account').click(); 
-  cy.contains('Password').click();
-  cy.url().should('include', '/account/password');
-});
-
-//12. Button Api
-it('Should click on the Api button in the Swagger menu', () => {
-  cy.contains('Sqlverifier').click();
-  cy.contains('Swagger').click(); 
-  cy.contains('API').click();
-  cy.url().should('include', '/docs/docs');
-});
-
-//13. Button Sign out
-it('Should click on the Sign out button in the Account menu', () => {
-  cy.contains('Sqlverifier').click();
-  cy.contains('Account').click(); 
-  cy.contains('Sign out').click();
-  cy.url().should('include', '/logout');
-});
-
+    it('Should navigate to Home page after clicking on a specific element', () => {
+      cy.contains('specific-element').click();
+      cy.get('li[class="nav-item"]').should('be.visible').click();
+      cy.url().should('eq', 'https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc');
+    });
+//8.Docs menu
+    it('Should navigate to Docs page from Docs menu', () => {
+       
+        cy.get('#docs-menu').click();
+        cy.get('#docs-menu > div > a').click();
+        cy.url().should('include', '/docs/docs');
+        cy.get('#select', { timeout: 10000 }).should('not.be.empty');
+    });
+//9.Language
+    it('Should switch language and verify text', () => {
+      
+      cy.switchLanguage('English', 'Home');
+    cy.switchLanguage('Français', 'Accueil');
+    cy.switchLanguage('Русский', 'Главная');
+    cy.switchLanguage('Українська', 'Головна');
+    cy.switchLanguage('English', 'Home');
+//10.Settings
+    it('Should navigate to User settings page from Account menu', () => {
+        
+        cy.get('#account-menu').click();
+        cy.get('#account-menu > div > a:nth-child(1)').click();
+        cy.url().should('include', '/account/settings');
+        cy.get('#settings-title').should('include.text', 'User settings for');
+    });
+//11. Password
+    it('Should navigate to Password page from Account menu', () => {
+        cy.get('#account-menu').click();
+        cy.get('[data-cy="passwordItem"]').click();
+        cy.url().should('include', '/account/password');
+        cy.get('#password-title').should('include.text', 'Password for');
+    });
+//12.Logout
+    it('Should logout', () => {
+        cy.logout();
+    });
+})
+})
